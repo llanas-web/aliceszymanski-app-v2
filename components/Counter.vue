695406865e557d9526c93c75e7e9a775b952f1f7
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2022-02-15 11:55:56 +0100

    Initialize feat/middleware_auth_api

---
+++ components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center h-full space-y-4 text-xl"
+  >
+    <p>Here's a counter:</p>
+    <div class="flex items-center justify-center">
+      <button
+        class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700"
+        @click="counter++"
+      >
+        +
+      </button>
+      <span class="mx-2">{{ counter }}</span>
+      <button
+        class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700"
+        @click="counter--"
+      >
+        -
+      </button>
+      <p>Should result as error in workflow</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+</script>
