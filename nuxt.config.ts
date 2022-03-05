722c2f457bc151d108cd5a39c1de812652f99247
Boris MAURENCE <b.maurence@gmail.com>
2022-03-05 16:39:42 +0100

    chore: lint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/styles.css"],
+  css: ['@/assets/css/styles.css'],
   build: {
     postcss: {
-      postcssOptions: require("./postcss.config.js"),
+      postcssOptions: require('./postcss.config.js'),
     },
   },
 });
